59f40a633599fafdb4306066c172c7fbaba42b0a
Ngô Quang Sơn <lethidieuthao22051995@ngothinham.edu.vn>
2022-06-26 20:54:15 +0700

    Initial solution and repository

---
+++ eShopSolution.Data/Entities/Categories.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Entities
+{
+    class Categories
+    {   
+        public int Id { set; get; }
+        public int SortOrder { set; get; }
+        public bool IsShowOnHome { }
+    }
+}
